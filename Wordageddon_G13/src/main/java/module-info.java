8a04811956a996409122c386bff4c42a11864079
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-07-05 11:19:39 +0200

    Creazione progetto vuoto

---
+++ Wordageddon_G13/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module mychat.wordageddon_g13 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens mychat.wordageddon_g13 to javafx.fxml;
+    exports mychat.wordageddon_g13;
+}
+*/
---
+++ Wordageddon_G13/src/main/java/mychat/wordageddon_g13/SecondaryController.java
@@ -0,0 +1,12 @@
+package mychat.wordageddon_g13;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
