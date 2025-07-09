eb4dade1c04ea93d9074a2e81b809304f3eb38aa
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-07-09 17:18:10 +0200

    implementazione metodo addFiles()

--- Wordageddon_G13/src/main/java/model/files_management/FileManager.java
+++ Wordageddon_G13/src/main/java/model/files_management/FileManager.java
@@ -1,20 +1,32 @@
 package model.files_management;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
+import java.nio.file.StandardCopyOption;
 
 public  class FileManager {
 
     private final static Path FILES_DIR_PATH = Paths.get("/data/files/");
 
     /**
-     * Aggiunge alla cartella "files" i file selezionati dall'admin
+     * Aggiunge alla cartella "files" i file selezionati dall'admin, sostituendo quelli già presenti aventi
+     * lo stesso nome dei nuovi
      * @param files lista dei file da aggiungere
+     * @throws IOException se non riesce a creare l'albero di cartelle previste
      */
-    public static void addFiles(List<File> files) {
+    public static void addFiles(List<File> files) throws IOException {
+       if(!Files.isDirectory(FILES_DIR_PATH)) Files.createDirectories(FILES_DIR_PATH);
 
+        for(File file : files) {
+
+            Path target = FILES_DIR_PATH.resolve(file.getName());
+            Files.copy(file.toPath(), target, StandardCopyOption.REPLACE_EXISTING);
+        }
     }
 
     /**
