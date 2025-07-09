255d7f53410864b178f6b8a97328e5a273bee2aa
Paolo <paolo32v@gmail.com>
2025-07-09 12:15:29 +0200

    k

--- Wordageddon_G13/src/main/java/controller/AuthMenuController.java
+++ Wordageddon_G13/src/main/java/controller/AuthMenuController.java
@@ -1,6 +1,58 @@
 package controller;
 
-// gestisce schermata di autenticazione
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+import javafx.event.ActionEvent;
 
 public class AuthMenuController {
+
+    @FXML
+    private Pane loginBtn;
+
+    @FXML
+    private TextField nomeUtenteTF;
+
+    @FXML
+    private TextField passwordTF;
+
+    @FXML
+    private RadioButton signInBtn;
+
+    @FXML
+    private RadioButton signUpBtn;
+
+    @FXML
+    private Label wordaggedonLabel;
+
+    @FXML
+    private Button loginButton;
+
+    private ToggleGroup loginToggleGroup;
+
+    @FXML
+    public void initialize() {
+        loginToggleGroup = new ToggleGroup();
+        signInBtn.setToggleGroup(loginToggleGroup);
+        signUpBtn.setToggleGroup(loginToggleGroup);
+
+        signInBtn.setSelected(true);
+    }
+
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        String username = nomeUtenteTF.getText();
+        String password = passwordTF.getText();
+
+        if (signInBtn.isSelected()) {
+            //verificare credenziali nel db
+        } else if (signUpBtn.isSelected()) {
+            //verificare credenziali nel db e se non esistono aggiungere
+        }
+    }
+
 }
