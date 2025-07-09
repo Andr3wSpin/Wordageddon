05bf9fcb5d5baa27e9377c8eee3d86a44c9f2053
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-07-09 20:01:34 +0200

    completamento classe FileManager

--- Wordageddon_G13/src/main/java/model/files_management/FileManager.java
+++ Wordageddon_G13/src/main/java/model/files_management/FileManager.java
@@ -8,41 +8,64 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import java.nio.file.StandardCopyOption;
+import java.util.stream.Collectors;
 
+/**
+ * Gestisce la logica per aggiungere o eliminare i file utilizzati dal sistema e permette di ottenere una
+ * lista di tutti i documenti presenti nella cartella "files"
+ */
 public  class FileManager {
 
     private final static Path FILES_DIR_PATH = Paths.get("data/files/");
 
     /**
-     * Aggiunge alla cartella "files" i file selezionati dall'admin, sostituendo quelli già presenti aventi
+     * Aggiunge alla cartella "files" i documenti selezionati dall'admin sostituendo quelli già presenti aventi
      * lo stesso nome dei nuovi
-     * @param files lista dei file da aggiungere
-     * @throws IOException se non riesce a creare l'albero di cartelle previste
+     * @param files lista dei documenti da aggiungere
+     * @throws IOException se non riesce a creare l'albero di cartelle previste o se non riesce a copiare
+     * i file nella cartella di destinazione
      */
     public static void addFiles(List<File> files) throws IOException {
 
        if(!Files.isDirectory(FILES_DIR_PATH)) Files.createDirectories(FILES_DIR_PATH);
 
-        for(File file : files) {
+       for(File file : files) {
 
-            Path target = FILES_DIR_PATH.resolve(file.getName());
-            Files.copy(file.toPath(), target, StandardCopyOption.REPLACE_EXISTING);
-        }
+           Path target = FILES_DIR_PATH.resolve(file.getName());
+           Files.copy(file.toPath(), target, StandardCopyOption.REPLACE_EXISTING);
+       }
     }
 
     /**
-     * Elimina dalla cartella "files" i file selezionati dall'admin
+     * Elimina dalla cartella "files" i documenti selezionati dall'admin
      * @param files lista dei file da eliminare
+     * @throws IOException se non riesce ad eliminare i file
      */
-    public static void deleteFiles(List<File> files) {
-        if(!Files.isDirectory(FILES_DIR_PATH)) return;
+    public static void deleteFiles(List<File> files) throws IOException {
 
-        for(File file : files) Files.deleteIfExists(file.toPath());
+        if (!Files.isDirectory(FILES_DIR_PATH)) return;
+
+        for (File file : files) {
+
+            Path target = file.toPath();
+
+            if (target.startsWith(FILES_DIR_PATH)) {
+
+                Files.deleteIfExists(target);
+            }
+        }
     }
 
     /**
      * Ottiene una lista contenente tutti i file salvati nella cartella "files"
      * @return lista di file salvati in memoria
+     * @throws IOException se non riesce ad ottenere i documenti presenti nella cartella
      */
-    public static List<File> getFiles() { return null; }
+    public static List<File> getFiles() throws IOException {
+
+        return Files.walk(FILES_DIR_PATH)
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+    }
 }
--- Wordageddon_G13/src/test/java/model/files_management/FileManagerTest.java
+++ Wordageddon_G13/src/test/java/model/files_management/FileManagerTest.java
@@ -11,37 +11,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileManagerTest {
 
-    private final String file1 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file1.txt";
-    private final String file2 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file2.txt";
-    private final String file3 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file3.txt";
-    private final String file4 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file4.txt";
+    private static final String file1 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file1.txt";
+    private static final String file2 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file2.txt";
+    private static final String file3 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file3.txt";
+    private static final String file4 = "/C:/Users/andre/OneDrive/Desktop/Test_Progetto_Java/file4.txt";
 
-    private List<File> files;
+    private static final String file5 = "/C:/Users/andre/Documents/GitHub/Wordageddon/Wordageddon_G13/data/files/file1.txt/";
+    private static final String file6 = "/C:/Users/andre/Documents/GitHub/Wordageddon/Wordageddon_G13/data/files/file2.txt/";
+    private static final String file7 = "/C:/Users/andre/Documents/GitHub/Wordageddon/Wordageddon_G13/data/files/file3.txt/";
+    private static final String file8 = "/C:/Users/andre/Documents/GitHub/Wordageddon/Wordageddon_G13/data/files/file4.txt/";
 
-    @BeforeEach
-    public void setUp() {
+    private static List<File> files1;
+    private static List<File> files2;
 
-        files = new ArrayList<>();
-        files.add(new File(file1));
-        files.add(new File(file2));
-        files.add(new File(file3));
-        files.add(new File(file4));
+    @BeforeAll
+    public static void setUp() {
+
+        files1 = new ArrayList<>();
+        files1.add(new File(file1));
+        files1.add(new File(file2));
+        files1.add(new File(file3));
+        files1.add(new File(file4));
+
+        files2 = new ArrayList<>();
+        files2.add(new File(file5));
+        files2.add(new File(file6));
+        files2.add(new File(file7));
+        files2.add(new File(file8));
     }
 
     @Test
     void addFiles() {
         try {
-            FileManager.addFiles(files);
+            FileManager.addFiles(files1);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Test
-    void deleteFiles() {
-    }
+//    @Test
+//    void deleteFiles() {
+//        try {
+//            FileManager.deleteFiles(files2);
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 
     @Test
     void getFiles() {
+
+        List<File> fileList = null;
+        try {
+            fileList = FileManager.getFiles();
+        } catch (IOException e) {
+            System.out.println("Errore durante l'ottenimento dei file");
+        }
+
+        if(fileList == null) return;
+
+        StringBuilder sb = new StringBuilder("FILES PRESENTI NELLA CARTELLA\n");
+        for(File file : fileList) {
+            sb.append(file.getName() + "\n");
+        }
+
+        System.out.println(sb);
     }
 }
