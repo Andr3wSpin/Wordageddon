e1467479b81c0ce38500ac94066d35914a5f4688
alex17-T <a.senatore151@studenti.unisa.it>
2025-07-17 19:32:07 +0200

    aggiunte animazioni e stile

--- Wordageddon_G13/src/main/java/controller/ProfileViewController.java
+++ Wordageddon_G13/src/main/java/controller/ProfileViewController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,11 +10,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.User;
 import model.db.WordageddonDAOSQLite;
 
@@ -29,12 +33,17 @@ import javafx.scene.layout.StackPane;
  * incluse le modifiche dei dati utente e il logout.
  */
 public class ProfileViewController implements Initializable {
-
     @FXML
     private Button Button_LogOut;
 
     @FXML
     private Button ChangeDataButton;
+
+    @FXML
+    private Button ChangeDataButton1;
+
+    @FXML
+    private Label LabelDelete;
 
     @FXML
     private Label Label_ChangeData;
@@ -43,28 +52,40 @@ public class ProfileViewController implements Initializable {
     private MediaView MediaViewWallPaper;
 
     @FXML
-    private Text textField_NomeUser;
-
-    @FXML
-    private TextField newAttributeTextField;
+    private Text Welcome;
 
     @FXML
     private Button confirmButton;
 
     @FXML
+    private StackPane deleteAccountButton;
+
+    @FXML
+    private ImageView iconChangeInfo;
+
+    @FXML
+    private ImageView iconDelete;
+
+    @FXML
+    private ImageView iconLogOut;
+
+    @FXML
+    private Label labelLogOut;
+
+    @FXML
     private Label newAttributeLabel;
+
+    @FXML
+    private TextField newAttributeTextField;
 
     @FXML
     private RadioButton passwordToggle;
 
     @FXML
+    private Text textField_NomeUser;
+
+    @FXML
     private RadioButton usernameToggle;
-
-    @FXML
-    private ImageView image_Logout;
-
-    @FXML
-    private Label labelLogOut;
 
     private ToggleGroup dataToggles;
 
@@ -72,17 +93,12 @@ public class ProfileViewController implements Initializable {
 
     private WordageddonDAOSQLite accessDB;
 
-    @FXML
-    private Text Welcome;
+
 
     @FXML
     private Label Label_ChangeData1;
 
-    @FXML
-    private Button ChangeDataButton1;
 
-    @FXML
-    private StackPane deleteAccountButton;
 
     @FXML
     private Label Label_ChangeData11;
@@ -262,5 +278,92 @@ public class ProfileViewController implements Initializable {
             // Se l'utente annulla, consuma l'evento per evitare ulteriori azioni
             event.consume();
         }
+    }
+
+
+    /**
+     * gestione animazioni pulsanti entred
+     * @param x icona da animare
+     * @param y label da animare
+     *
+     */
+    private void buttonHoverEntredAnimator(ImageView x,Label y){
+
+        TranslateTransition textSlide = new TranslateTransition(Duration.millis(500),y);
+        textSlide.setFromX(0);
+        textSlide.setToX(100);
+        textSlide.play();
+
+        FadeTransition textOpacity = new FadeTransition(Duration.millis(500),y);
+        textOpacity.setFromValue(1.0);
+        textOpacity.setToValue(0.0);
+        textOpacity.play();
+
+        TranslateTransition imageSlide = new TranslateTransition(Duration.millis(300),x);
+        imageSlide.setFromX(0);
+        imageSlide.setToX(35);
+        imageSlide.play();
+
+
+
+    }
+
+    /**
+     * gestione animazioni pulsanti exit
+     * @param x icona da animare
+     * @param y label da animare
+     *
+     */
+    private void buttonHoverExitAnimator(ImageView x,Label y){
+
+        TranslateTransition textSlide = new TranslateTransition(Duration.millis(500),y);
+        textSlide.setFromX(100);
+        textSlide.setToX(0);
+        textSlide.play();
+
+        FadeTransition textOpacity = new FadeTransition(Duration.millis(500),y);
+        textOpacity.setFromValue(0);
+        textOpacity.setToValue(1);
+        textOpacity.play();
+
+        TranslateTransition imageSlide = new TranslateTransition(Duration.millis(300),x);
+        imageSlide.setFromX(35);
+        imageSlide.setToX(0);
+        imageSlide.play();
+
+
+
+    }
+
+
+
+    @FXML
+    void hoverButtonChangeInfoEntred(MouseEvent event) {
+                buttonHoverEntredAnimator(iconChangeInfo,Label_ChangeData);
+    }
+
+    @FXML
+    void hoverButtonChangeInfoExit(MouseEvent event) {
+        buttonHoverExitAnimator(iconChangeInfo,Label_ChangeData);
+    }
+
+    @FXML
+    void hoverButtonDeleteEntred(MouseEvent event) {
+        buttonHoverEntredAnimator(iconDelete,LabelDelete);
+    }
+
+    @FXML
+    void hoverButtonDeleteExit(MouseEvent event) {
+        buttonHoverExitAnimator(iconDelete,LabelDelete);
+    }
+
+    @FXML
+    void hoverButtonLogOutEntred(MouseEvent event) {
+          buttonHoverEntredAnimator(iconLogOut,labelLogOut);
+    }
+
+    @FXML
+    void hoverButtonLogOutExit(MouseEvent event) {
+        buttonHoverExitAnimator(iconLogOut,labelLogOut);
     }
 }
