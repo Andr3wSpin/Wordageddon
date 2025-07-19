3d373499cbcc1949291e6a28880f713abbb9fe59
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-07-19 12:29:30 +0200

    .

--- Wordageddon/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module mychat.wordageddon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens mychat.wordageddon to javafx.fxml;
-    exports mychat.wordageddon;
-}
-*/
